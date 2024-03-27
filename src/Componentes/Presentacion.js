d5a3842c3ad4c4a13e0d770fdff6ef1f7b879cb0
panchocandioti <panchocandioti@gmail.com>
2024-03-27 16:59:46 -0300

    Instrucciones

--- src/Componentes/Presentacion.js
+++ src/Componentes/Presentacion.js
@@ -57,25 +57,80 @@ function Presentacion() {
                         {mostrarGeneralidades === true ? "Generalidades y objetivos ˄ " : "Generalidades y objetivos ˅ "}
                     </button>
                     {mostrarGeneralidades && (<div className='contenido-instrucciones'>
-                        <p>INSTRUCCIONES</p>
+                        <p>La producción de leche es una actividad compleja y cambiante. Debido a la gran cantidad de variables que involucra,
+                            el cálculo de resultados económicos puede resultar difícil. El análisis de los factores determinantes del resultado y
+                            de su importancia relativa es, por consiguiente, más dificultoso aún.</p>
+                        <p><b>Objetivo de esta aplicación:</b></p>
+                        <p><b>Estimar el resultado económico de un establecimiento lechero en forma sencilla y rápida, permitiendo además el
+                            análisis de los distintos factores que lo determinan.</b></p>
+                        <p>La aplicación puede utilizarse para:</p>
+                        <ul>
+                            <li>Evaluar situaciones desconocidas o hipotéticas</li>
+                            <li>Diagnosticar situaciones en marcha</li>
+                            <li>Encontrar rápido los rubros de mayor impacto en el resultado</li>
+                            <li>Evaluar situaciones alternativas</li>
+                            <li>Uso didáctico en general</li>
+                        </ul>
+                        <p>El usuario deberá ingresar parámetros relativamente simples, divididos en seis pasos:</p>
+                        <ol>
+                            <li>Indicadores físicos</li>
+                            <li>Ingresos brutos</li>
+                            <li>Gasto en mano de obra</li>
+                            <li>Gasto de reposición</li>
+                            <li>Otros gastos directos</li>
+                            <li>Gastos de estructura</li>
+                        </ol>
+                        <p>Durante la carga, para cada input podrá consultarse una definición simplificada o un breve
+                            mensaje de ayuda. Los valores ingresados podrán cambiarse en cualquier momento.</p>
+                        <p>Es importante entender que esta aplicación está concebida como una calculadora
+                            (facilita y ordena el cálculo y la interpretación de resultados). No es un modelo de simulación
+                            (no simula procesos). La calidad de la información ingresada por el usuario es su entera
+                            responsabilidad y condiciona la calidad de los resultados obtenidos.</p>
                     </div>)}
                     <br></br>
                     <button onClick={manejarOnClick4} className='mostrar'>
                         {mostrarResultados === true ? "Resultados que se obtienen ˄ " : "Resultados que se obtienen ˅ "}
                     </button>
                     {mostrarResultados && (<div className='contenido-instrucciones'>
-                        <p>
-                            RESULTADOS
-                        </p>
+                        <p>Parciales:</p>
+                        <ul>
+                            <li>Resumen de indicadores físicos</li>
+                            <li>Resumen de ingresos brutos</li>
+                            <li>Resumen de mano de obra</li>
+                            <li>Resumen de reposición</li>
+                            <li>Resumen de otros gastos directos</li>
+                            <li>Resumen de gastos de estructura</li>
+                        </ul>
+                        <p>Finales:</p>
+                        <ul>
+                            <li>Resultado operativo – Análisis por litro</li>
+                            <li>Resultado operativo – Montos anuales y porcentuales del Ingreso Bruto</li>
+                            <li>Otras expresiones de resultado operativo</li>
+                            <li>Gráficos de distribución del Ingreso Bruto</li>
+                        </ul>
+                        <p>Los resultados, parciales y finales, podrán visualizarse expresados en distintas unidades,
+                            para facilitar el análisis (anuales, mensuales, % del IB, % del IB Leche, por litro,
+                            por hectárea, por vaca), según su naturaleza.</p>
+                        <p>A su vez, todas las expresiones en dinero podrán visualizarse en dos monedas diferentes,
+                            a elección del usuario.</p>
+                        <p>Se podrá descargar un reporte en PDF con todos los resultados. El reporte se emite en la
+                            moneda activa al momento de generarlo. Si se realizan cambios posteriores (incluyendo la moneda
+                            activa) pueden volver a generarse nuevos reportes.</p>
                     </div>)}
                     <br></br>
                     <button onClick={manejarOnClick5} className='mostrar'>
                         {mostrarSecuencia === true ? "Secuencia de trabajo sugerida ˄ " : "Secuencia de trabajo sugerida ˅ "}
                     </button>
                     {mostrarSecuencia && (<div className='contenido-instrucciones'>
-                        <p>
-                            SECUENCIA
-                        </p>
+                        <ol>
+                            <li>Diagnóstico (situación inicial)</li>
+                            <li>Generación de reporte PDF inicial</li>
+                            <li>Detección de puntos críticos</li>
+                            <li>¿Qué pasaría si...? (pruebas)</li>
+                            <li>Selección de alternativa exitosa</li>
+                            <li>Análisis de los resultados y ajustes</li>
+                            <li>Generación de reporte PDF final</li>
+                        </ol>
                     </div>)}
                     <br></br>
                     <button onClick={manejarOnClick6} className='mostrar'>
